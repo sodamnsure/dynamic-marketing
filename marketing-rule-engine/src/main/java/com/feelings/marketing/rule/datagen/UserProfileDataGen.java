29c6754c7eada181459f44d4f8a0c8680334a7d8
sodamnsure <zhaiguoweii5@163.com>
2021-05-27 19:43:49 +0800

    创建用户画像数据模拟器程序

---
+++ marketing-rule-engine/src/main/java/com/feelings/marketing/rule/datagen/UserProfileDataGen.java
@@ -0,0 +1,13 @@
+package com.feelings.marketing.rule.datagen;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2021/5/27 7:36 下午
+ * @desc: 用户画像数据模拟器：
+ * 用户画像常见对应两个英文词Personas和User Profile,Personas属于交互设计领域的概念,后者常用于营销领域。
+ */
+public class UserProfileDataGen {
+    public static void main(String[] args) {
+
+    }
+}
