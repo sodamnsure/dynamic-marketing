ce3d97231dfb4fc5a967851fb5f04fc8d63ca0f7
sodamnsure <zhaiguoweii5@163.com>
2021-05-31 10:53:15 +0800

    flink测试接收kafka数据源

--- marketing-rule-engine/src/main/java/com/feelings/marketing/rule/engine/RuleEngineV1.java
+++ marketing-rule-engine/src/main/java/com/feelings/marketing/rule/engine/RuleEngineV1.java
@@ -1,5 +1,13 @@
 package com.feelings.marketing.rule.engine;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
 /**
  * @Author: sodamnsure
  * @Date: 2021/5/28 2:27 下午
@@ -12,7 +20,24 @@ package com.feelings.marketing.rule.engine;
  *  行为次序条件：依次做过--> W(p1=v4) --> R(p2 = v3) --> F
  */
 public class RuleEngineV1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // 创建Environment
+        Configuration conf = new Configuration();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+
+        // 添加kafka数据源
+        Properties prop = new Properties();
+        prop.setProperty("bootstrap.servers", "feelings:9092");
+        prop.setProperty("auto.offset.reset", "latest");
+        FlinkKafkaConsumer<String> kafkaSource = new FlinkKafkaConsumer<>("ActionLog", new SimpleStringSchema(), prop);
+
+        // 将数据源添加到Environment
+        DataStreamSource<String> logStream = env.addSource(kafkaSource);
+
+        // 打印测试
+        logStream.print();
+
+        env.execute();
 
     }
 }
