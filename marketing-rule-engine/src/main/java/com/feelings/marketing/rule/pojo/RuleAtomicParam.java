8e4e5d5a9513ef02e98526b106c1bbfd809c63bd
sodamnsure <zhaiguoweii5@163.com>
2021-06-02 15:36:29 +0800

    规则参数条件封装类创建

---
+++ marketing-rule-engine/src/main/java/com/feelings/marketing/rule/pojo/RuleAtomicParam.java
@@ -0,0 +1,12 @@
+package com.feelings.marketing.rule.pojo;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2021/6/2 3:35 下午
+ * @desc: 规则参数中的原子条件封装实体
+ */
+public class RuleAtomicParam {
+    public static void main(String[] args) {
+
+    }
+}
