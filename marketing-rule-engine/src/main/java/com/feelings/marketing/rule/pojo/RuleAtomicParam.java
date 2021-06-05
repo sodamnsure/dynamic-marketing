e2f674a2ea3ad6b724ad48b0d3ae1364185a67f3
sodamnsure <zhaiguoweii5@163.com>
2021-06-05 10:50:52 +0800

    规则参数条件封装类提交

--- marketing-rule-engine/src/main/java/com/feelings/marketing/rule/pojo/RuleAtomicParam.java
+++ marketing-rule-engine/src/main/java/com/feelings/marketing/rule/pojo/RuleAtomicParam.java
@@ -1,12 +1,29 @@
 package com.feelings.marketing.rule.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
 /**
  * @Author: sodamnsure
  * @Date: 2021/6/2 3:35 下午
  * @desc: 规则参数中的原子条件封装实体
  */
-public class RuleAtomicParam {
-    public static void main(String[] args) {
-
-    }
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RuleAtomicParam implements Serializable {
+    // 事件类型
+    private String eventId;
+    // 事件属性要求
+    private HashMap<String, String> properties;
+    // 阈值要求
+    private int threshold;
+    // 要求的事件发生时间段起始时间
+    private Long rangeStart;
+    // 要求的事件发生时间段结束时间
+    private Long rangeEnd;
 }
