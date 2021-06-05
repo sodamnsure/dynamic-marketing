1dbc420cef9b8bca8e66d9115332a5d245759285
sodamnsure <zhaiguoweii5@163.com>
2021-06-05 11:00:07 +0800

    规则整体条件封装实体创建提交

---
+++ marketing-rule-engine/src/main/java/com/feelings/marketing/rule/pojo/RuleParam.java
@@ -0,0 +1,28 @@
+package com.feelings.marketing.rule.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2021/6/5 10:52 上午
+ * @desc: 规则整体条件封装实体
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RuleParam implements Serializable {
+    // 规则中的触发条件
+    private RuleAtomicParam triggerParam;
+    // 规则中的用户画像条件
+    private HashMap<String, String> userProfileParams;
+    // 规则中的行为属性条件
+    private List<RuleAtomicParam> userActionParams;
+    // 规则中的行为次序条件
+    private List<RuleAtomicParam> userActionSeqParams;
+}
