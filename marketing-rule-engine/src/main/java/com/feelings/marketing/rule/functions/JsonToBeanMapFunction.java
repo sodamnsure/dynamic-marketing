44f6e1358ba60a936f52b9926daeae4d5a855566
sodamnsure <zhaiguoweii5@163.com>
2021-06-16 18:11:58 +0800

    实现MapFunction接口，将Json数据转换成Pojo对象

--- marketing-rule-engine/src/main/java/com/feelings/marketing/rule/functions/JsonToBeanMapFunction.java
+++ marketing-rule-engine/src/main/java/com/feelings/marketing/rule/functions/JsonToBeanMapFunction.java
@@ -1,6 +1,7 @@
 package com.feelings.marketing.rule.functions;
 
 import com.feelings.marketing.rule.pojo.LogBean;
+import com.feelings.marketing.rule.utils.JsonUtils;
 import org.apache.flink.api.common.functions.MapFunction;
 
 /**
@@ -10,7 +11,7 @@ import org.apache.flink.api.common.functions.MapFunction;
 public class JsonToBeanMapFunction implements MapFunction<String, LogBean> {
 
     @Override
-    public LogBean map(String s) throws Exception {
-        return null;
+    public LogBean map(String value) throws Exception {
+        return JsonUtils.jsonToPojo(value, LogBean.class);
     }
 }
