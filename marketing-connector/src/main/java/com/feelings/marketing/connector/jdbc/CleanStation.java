e7df900e0f9eddafe02e5ddf6dc4293a1ead9663
sodamnsure <zhaiguoweii5@163.com>
2021-06-18 16:54:40 +0800

    创建connector项目

---
+++ marketing-connector/src/main/java/com/feelings/marketing/connector/jdbc/CleanStation.java
@@ -0,0 +1,13 @@
+package com.feelings.marketing.connector.jdbc;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @Author: sodamnsure
+ * @Date: 2021/6/18 4:50 下午
+ * @desc: 清洗电站数据
+ */
+public class CleanStation {
+    public static void main(String[] args) {
+
+    }
+}
