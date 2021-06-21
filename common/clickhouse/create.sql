01fb7723a7fe21e39e77b3e2a6e2198e87ff089a
sodamnsure <zhaiguoweii5@163.com>
2021-06-21 15:12:43 +0800

    创建事件明细表和物化视图，将kafka中的数据存储到clickhouse中

--- common/clickhouse/create.sql
+++ common/clickhouse/create.sql
@@ -1,25 +1,77 @@
-set allow_experimental_map_type = 1;
-create table default.event_detail_kafka (
-                                            account String,
-                                            appId String,
-                                            appVersion String,
-                                            carrier String,
-                                            deviceId String,
-                                            deviceType String,
-                                            ip String,
-                                            latitude Float32,
-                                            longitude Float32,
-                                            netType String,
-                                            osName String,
-                                            osVersion String,
-                                            releaseChannel String,
-                                            resolution String,
-                                            sessionId String,
-                                            timeStamp UInt64,
-                                            eventId String,
-                                            properties Map(String, String)
+set
+allow_experimental_map_type = 1;
+
+-- 创建kafka引擎表
+create table default.event_detail_kafka
+(
+    account        String,
+    appId          String,
+    appVersion     String,
+    carrier        String,
+    deviceId       String,
+    deviceType     String,
+    ip             String,
+    latitude       Float32,
+    longitude      Float32,
+    netType        String,
+    osName         String,
+    osVersion      String,
+    releaseChannel String,
+    resolution     String,
+    sessionId      String,
+    timeStamp      UInt64,
+    eventId        String,
+    properties     Map(String, String)
 ) engine = Kafka('47.94.223.241:9092', 'ActionLog', 'group1', 'JSONEachRow');
 
 
-select *
+-- 创建事件明细表
+create table default.event_detail
+(
+    account        String,
+    appId          String,
+    appVersion     String,
+    carrier        String,
+    deviceId       String,
+    deviceType     String,
+    ip             String,
+    latitude       Float32,
+    longitude      Float32,
+    netType        String,
+    osName         String,
+    osVersion      String,
+    releaseChannel String,
+    resolution     String,
+    sessionId      String,
+    timeStamp      UInt64,
+    eventId        String,
+    properties     Map(String, String),
+    index          u (deviceId) type minmax granularity 3,
+    index          t ( timeStamp) type minmax granularity 3
+) engine = MergeTree()
+order by (deviceId, timeStamp);
+
+
+-- 创建物化视图
+create
+materialized view default.event_view to default.event_detail
+as
+select account,
+       appId,
+       appVersion,
+       carrier,
+       deviceId,
+       deviceType,
+       ip,
+       latitude,
+       longitude,
+       netType,
+       osName,
+       osVersion,
+       releaseChannel,
+       resolution,
+       sessionId,
+       timeStamp,
+       eventId,
+       properties
 from default.event_detail_kafka;
